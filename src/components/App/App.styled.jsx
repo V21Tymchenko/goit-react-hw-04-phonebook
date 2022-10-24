024a174c86ef440aa21202474490f16c2b569826
Valeriia tymchenko <lera.tymchenko@ukr.net>
2022-10-24 19:07:43 +0300

    add styled system base files

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { space, layout } from 'styled-components';
+import { space, layout } from 'styled-system';
 export const Box = styled('div')(
   {
     boxSizing: 'border-box',
